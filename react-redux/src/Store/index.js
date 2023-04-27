27d295f9dfd632068915137e974c86b0cda595e1
Dharvik19 <dharvik.badga22@gmail.com>
2023-04-27 18:20:38 +0530

    added seperate authentication slice

--- react-redux/src/Store/index.js
+++ react-redux/src/Store/index.js
@@ -1,5 +1,5 @@
 // import {createStore} from 'redux';
-import { legacy_createStore as createStore} from 'redux'
+// import { legacy_createStore as createStore} from 'redux'
 import {createSlice, configureStore} from '@reduxjs/toolkit';
 const initialState = {counter:0, showCounter: true};
 const counterSlice = createSlice({
@@ -23,6 +23,21 @@ const counterSlice = createSlice({
         }
     }
 
+})
+const initialAuthState = {
+    isAuthenticated: false
+}
+const authSlice =createSlice({
+    name: 'authentication',
+    initialState:initialAuthState,
+    reducers:{
+        login(state){
+            state.isAuthenticated = true;
+        },
+        logOut(state){
+            state.isAuthenticated = false;
+        },
+    }
 })
 // const counterReducer = (state = initialState, action)=>{
 //     if(action.type === 'increment'){
@@ -61,8 +76,11 @@ const counterSlice = createSlice({
 // const store   = createStore(counterSlice.reducer);
 
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {counter: counterSlice.reducer, auth:authSlice.reducer
+    }
 })
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
+
 export default store;
--- react-redux/src/components/Counter.js
+++ react-redux/src/components/Counter.js
@@ -1,42 +1,42 @@
 import classes from './Counter.module.css';
 import {useSelector, useDispatch} from 'react-redux';
-
+import {counterActions}from '../Store/index'
 const Counter = () => {
-  const counter = useSelector(state => state.counter)
+  const counter = useSelector(state => state.counter.counter);// first counter is the property name we chose in reducer object second counter is the initial counter state
+  const show = useSelector(state=> state.counter.showCounter);
   // need to pass a function to useSelector which will determine what data we are looking; for a slice
   // it automatically suscribes the store and updates the state whenever there is a state change ans if we unmount the component it also clea the subscription
   const dispatch = useDispatch();
 
   const incrementHandler =()=>{
-    dispatch( {
-      type: 'increment'
-    })
+    dispatch(counterActions.increment())
   }
   const dreementHandler =()=>{
-    dispatch( {
-      type: 'decrement'
-    })
+    dispatch(counterActions.decrement())
   }
   const incrementby5handler = ()=>{
     dispatch(
-      {
-        type:'incrementby5'
-      }
+      counterActions.incrementBy5(5)
     )
   }
   const decrementby5handler = ()=>{
     dispatch(
-      {
-        type:'decrementby5'
-      }
+      counterActions.decrementBy5(5)
     )
   }
-  const toggleCounterHandler = () => {};
+  // const toggleCounterHandler = () => {
+  //   dispatch({
+  //     type: 'toggle'
+  //   })
+  // };
+  const toggleCounterHandler = () => {
+    dispatch(counterActions.toggleCounter());
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
       <button onClick={incrementHandler}>Increment</button>
       <button onClick={dreementHandler}>Decrement</button>
